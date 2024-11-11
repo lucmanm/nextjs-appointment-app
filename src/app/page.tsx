b099457eab2796afeb2049baaa3ca36caa997a5f
lucmanm <greenjamez22@gmail.com>
2024-11-12 00:55:28 +0300

    set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Hero from "@/components/hero";
+import { assets } from "@/constant/assets";
+import Image from "next/image";
+import { Fragment } from "react";
 
 export default function Home() {
-  return <Hero/>;
+  return (
+    <Fragment>
+      <Hero/>
+      <Image src={assets.appointment_img} alt="Image" width={100} height={100}/>
+    </Fragment>
+  );
 }
