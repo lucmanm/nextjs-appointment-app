8c8718dc731ffebba01db3670d0d72dc16bf6755
lucmanm <greenjamez22@gmail.com>
2024-12-11 00:02:20 +0300

    header updates

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'plus.unsplash.com',
-      //   port: '',
-      //   pathname: '/**',
-      // },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -24,28 +24,32 @@ export const menu = [
     path: "/contact-us",
   },
 ];
+
 export default function Header() {
   return (
     <header className="flex items-center p-4 border-b space-x-4 justify-between">
-      <MobileMenu />
-      <div className="flex space-x-6 items-center">
-        {/* Logo header icon */}
-        <IconHeader />
-        <NavigationMenu className="hidden md:flex">
-          <NavigationMenuList>
-            {menu.map((data, idx) => (
-              <NavigationMenuItem key={idx}>
-                <Link href={data.path} legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle({ className: "font-semibold " })}>
-                    {data.name}
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu>
+      <div className="container mx-auto flex justify-between max-sm:w-full">
+        <MobileMenu />
+        <div className="flex space-x-6 items-center">
+          {/* Logo header icon */}
+          <IconHeader />
+          {/* Navigation */}
+          <NavigationMenu className="hidden md:flex">
+            <NavigationMenuList>
+              {menu.map((data, idx) => (
+                <NavigationMenuItem key={idx}>
+                  <Link href={data.path} legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle({ className: "font-semibold " })}>
+                      {data.name}
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <Button> Get Started</Button>
       </div>
-      <Button> Get Started</Button>
     </header>
   );
 }
